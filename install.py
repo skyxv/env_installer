0cd9e88ef872db9273d5c4d7e827972ab3e3fd09
yandenghong <vincentyim8@163.com>
2019-08-26 15:53:54 +0800

    Added install, pack

---
+++ install.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+from src.env_installer import env_installer
+
+
+if __name__ == '__main__':
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+    env_installer.install()
+
